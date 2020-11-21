d0d294eb4b3403a5a3cf726dbcbef93afdd0d577
Ramon do Rosário <ramondorosario@gmail.com>
2020-11-21 19:02:54 -0300

    Funcionalidade: Customers page

--- src/pages/CustomersPage/index.js
+++ src/pages/CustomersPage/index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import IconSearch from "../../images/icon-search.svg";
+import React, { Children } from "react";
+import SearchIcon from "../../images/icon-search.svg";
+import EditIcon from "../../images/icon-edit.svg";
+import EmailIcon from "../../images/icon-email.svg";
+import TelephoneIcon from "../../images/icon-telephone.svg";
+import BackIcon from "../../images/icon-back.svg";
+import NextIcon from "../../images/icon-next.svg";
 
 import "./index.css";
 
@@ -12,7 +17,7 @@ export function CustomersPage() {
           <input placeholder="Procurar por Nome, E-mail ou CPF" />
           <button>
             <div>
-              <img src={IconSearch} alt="Botão de busca" />
+              <img src={SearchIcon} alt="Botão de busca" />
               Buscar
             </div>
           </button>
@@ -29,7 +34,90 @@ export function CustomersPage() {
               <th />
             </tr>
           </thead>
+          <tbody>
+            <tr>
+              <td className="datas">
+                <div>Nome e Sobrenome do cliente</div>
+                <div>
+                  <img src={EmailIcon} alt="Ícone email" />
+                  Nome e Sobrenome do cliente
+                </div>
+                <div>
+                  <img src={TelephoneIcon} alt="Ícone telefone" />
+                  Nome e Sobrenome do cliente
+                </div>
+              </td>
+              <td>R$ 00.000,00</td>
+              <td>R$ 00.000,00</td>
+              <td className="status">Em dia</td>
+              <td>
+                <button>
+                  <img src={EditIcon} alt="Icone editar cliente" />
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="datas">
+                <div>Nome e Sobrenome do cliente</div>
+                <div>
+                  <img src={EmailIcon} alt="Ícone email" />
+                  Nome e Sobrenome do cliente
+                </div>
+                <div>
+                  <img src={TelephoneIcon} alt="Ícone telefone" />
+                  Nome e Sobrenome do cliente
+                </div>
+              </td>
+              <td>R$ 00.000,00</td>
+              <td>R$ 00.000,00</td>
+              <td className="status">Inadimplente</td>
+              <td>
+                <button>
+                  <img src={EditIcon} alt="Icone editar cliente" />
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="datas">
+                <div>Nome e Sobrenome do cliente</div>
+                <div>
+                  <img src={EmailIcon} alt="Ícone email" />
+                  Nome e Sobrenome do cliente
+                </div>
+                <div>
+                  <img src={TelephoneIcon} alt="Ícone telefone" />
+                  Nome e Sobrenome do cliente
+                </div>
+              </td>
+              <td>R$ 00.000,00</td>
+              <td>R$ 00.000,00</td>
+              <td className="status">Pendente</td>
+              <td>
+                <button>
+                  <img src={EditIcon} alt="Icone editar cliente" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
         </table>
+        <div className="container-buttons">
+          <button className="back-page">
+            <img src={BackIcon} alt="Página anterior" />
+          </button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>10</button>
+          <button className="next-page">
+            <img src={NextIcon} alt="Próxima página" />
+          </button>
+        </div>
       </div>
     </>
   );
